de0bde927cd4f503ad3453e3d75f4077df90e097
Samarth MAlhotra <samarthmalhotra4200@gmail.com>
2025-12-14 20:41:13 +0530

    add latest update

--- src/Docs.jsx
+++ src/Docs.jsx
@@ -5,7 +5,7 @@ function Docs() {
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center m-2">
-        <h4 className="text-primary pt-3 pt-xs-4 mt-5 text-center text-decoration-underline fs-xs-1">
+        <h4 className="text-primary pt-3 pt-xs-4 mt-xs-5 mt-5 text-center text-decoration-underline fs-xs-1">
           Detail information about Decimal to Binary , Octal , Hexadecimal
           Conversions :-
         </h4>
